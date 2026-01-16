999d79b8abf148a855c5f7eec4f423989611de06
Ugalimayai <chrisephraim38@gmail.com>
2026-01-16 19:04:01 +0300

    Add improvements on the hello world introductory session program.

---
+++ .app/sess01_introduction/hello_world.py
@@ -0,0 +1,16 @@
+# First Python code to display "Hello World!" on the console
+print("Hello World!")
+
+name = "Chris"
+print(f"Your name is of {type(name)}")
+
+num1 = 8
+num2 = 9
+print(f"the sum of {num1} and {num2} is {num1 + num2}")
+
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,10]
+
+list1.extend(list2)
+print(list1)
+
